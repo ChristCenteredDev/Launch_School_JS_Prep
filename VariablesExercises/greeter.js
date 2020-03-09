2e53fdedb9a8aaf1bcf0a955e9cff4d392556ac9
William M <wmooney1984@gmail.com>
2020-03-08 21:37:49 -0400

    Variable Exercises Completed

---
+++ VariablesExercises/greeter.js
@@ -0,0 +1,9 @@
+let name = 'Victor';
+
+const greet = () => {
+  console.log(`Good Morning, ${name}.`);
+  console.log(`Good Afternoon, ${name}.`);
+  console.log(`Good Evening, ${name}.`);
+};
+
+greet();
